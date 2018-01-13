a840c7cee8be045cfcd7a0b5357d1f0720883159
FlorentE96 <flo.ecochard@gmail.com>
2018-01-13 02:26:44 -0200

    [Agent panel] Remade interface

--- Sources/AgentPanel.java
+++ Sources/AgentPanel.java
@@ -12,6 +12,7 @@ public class AgentPanel
     private Agent agent;
     
     private JTextField nameField, idField, salaryField, balanceField, salaryLevelField;
+    private JPanel mainPanel;
 
     public AgentPanel(Agent _agent, boolean isRegistering)
     {
@@ -21,12 +22,6 @@ public class AgentPanel
         JPanel infoPanel;
         infoPanel = new JPanel();
 
-        nameField = new JTextField(10);
-        idField = new JTextField(10);
-        salaryField = new JTextField(10);
-        balanceField = new JTextField(10);
-        salaryLevelField = new JTextField(10);
-
         nameField.setEditable(editable);
         idField.setEditable(false);
         salaryField.setEditable(editable);
@@ -35,65 +30,8 @@ public class AgentPanel
 
         this.updateValues();
 
-        JLabel nameLabel = new JLabel("Name:", JLabel.TRAILING);
-        JLabel idLabel = new JLabel("ID:", JLabel.TRAILING);
-        JLabel salaryLabel = new JLabel("Current Salary:", JLabel.TRAILING);
-        JLabel balanceLabel = new JLabel("Sales Balance:", JLabel.TRAILING);
-        JLabel salaryLevelLabel = new JLabel("Salary Level:", JLabel.TRAILING);
-
-        nameLabel.setLabelFor(nameField);
-        idLabel.setLabelFor(idField);
-        salaryLabel.setLabelFor(salaryField);
-        balanceLabel.setLabelFor(balanceField);
-        salaryLevelLabel.setLabelFor(salaryLevelField);
-
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
-        infoPanel.setLayout(new GridBagLayout());
-        GridBagConstraints layoutConstraints = new GridBagConstraints();
-
-        infoPanel.setOpaque(true);
-
-        layoutConstraints.fill = GridBagConstraints.NONE;
-        layoutConstraints.anchor = GridBagConstraints.NORTHWEST;
-        layoutConstraints.weightx = 0.5;
-        layoutConstraints.weighty = 0.5;
-        layoutConstraints.insets = new Insets(0,10,10,10);
-
-        layoutConstraints.gridx = 0;
-        layoutConstraints.gridy = 0;
-        infoPanel.add(idLabel, layoutConstraints);
-        layoutConstraints.gridx = 0;
-        layoutConstraints.gridy = 1;
-        infoPanel.add(nameLabel, layoutConstraints);
-        layoutConstraints.gridx = 0;
-        layoutConstraints.gridy = 2;
-        infoPanel.add(salaryLabel, layoutConstraints);
-        layoutConstraints.gridx = 0;
-        layoutConstraints.gridy = 3;
-        infoPanel.add(balanceLabel, layoutConstraints);
-        layoutConstraints.gridx = 0;
-        layoutConstraints.gridy = 4;
-        infoPanel.add(salaryLevelLabel, layoutConstraints);
-
-        layoutConstraints.fill = GridBagConstraints.HORIZONTAL;
-        layoutConstraints.gridx = 1;
-        layoutConstraints.gridy = 0;
-        infoPanel.add(idField, layoutConstraints);
-        layoutConstraints.gridx = 1;
-        layoutConstraints.gridy = 1;
-        infoPanel.add(nameField, layoutConstraints);
-        layoutConstraints.gridx = 1;
-        layoutConstraints.gridy = 2;
-        infoPanel.add(salaryField, layoutConstraints);
-        layoutConstraints.gridx = 1;
-        layoutConstraints.gridy = 3;
-        infoPanel.add(balanceField, layoutConstraints);
-        layoutConstraints.gridx = 1;
-        layoutConstraints.gridy = 4;
-        infoPanel.add(salaryLevelField, layoutConstraints);
-
-        this.add(infoPanel);
+        this.setLayout(new BorderLayout());
+        this.add(mainPanel);
     }
 
     public Agent getAgetFromForm()
